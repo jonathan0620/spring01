2e9d516925bf00169e9b668536c766997db6d6df
jonathan <0620jonathan@gmail.com>
2023-11-08 16:43:27 +0900

    스프링

--- src/main/java/com/multi/mvc01/BbsController.java
+++ src/main/java/com/multi/mvc01/BbsController.java
@@ -3,12 +3,28 @@ package com.multi.mvc01;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller //싱글톤 + 컨트롤러 등록 
 public class BbsController {
 
 	//요청하나당 함수하나.
 	@RequestMapping("insert2")
-	public void insert2() {
+	public void insert2(BbsDTO2 bag) {
+		System.out.println(bag);
 		//db처리 --> views/insert2.jsp결과를 출력해주세요.
+		BbsDAO dao = new BbsDAO();
+		int result = dao.insert(bag);
+		System.out.println(result);
+	}
+	
+	@RequestMapping("update2")
+	public String update2(BbsDTO2 bag) {
+		BbsDAO dao = new BbsDAO();
+	 	int result = dao.update(bag);
+	 	//views에 update2.jsp를 만드세요.
+	 	if(result == 1) {
+	 		return "ok";
+	 	}else{
+	 		return "no";
+	 	}
 	}
 }
---
+++ src/main/webapp/webapp/WEB-INF/views/insert2.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body bgcolor="yellow">
+게시판 글쓰기 요청됨. 글쓰기한 행 개수: ${result}<hr>
+<a href="bbs.jsp">게시판 목록으로 go!</a>
+</body>
+</html>
